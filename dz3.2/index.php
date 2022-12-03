332f1aaafa3e35215fa5fdfc9e1353ad70d4ca5e
SvetoslavSO <sshaunin@mail.ru>
2022-12-03 17:53:49 +0300

    2-nd homework corrected

--- dz3.2/index.php
+++ dz3.2/index.php
@@ -19,26 +19,55 @@ $comment = $_GET['comment'] ?? 0;
 $address = $street. '/' . $home . '/' . $part . '/' . $apprtament . '/' . $floor;
 
 if ($email) {
-  $ret = $pdo->prepare("SELECT * FROM `users` WHERE email=?");
-  $ret->execute([$email]);
-  $user = $ret->fetch();
-  if (!$user) {
-    $newRet = $pdo->prepare("INSERT INTO `users` (`id`, `email`, `orders`, `phone`, `name`) VALUES(?, ?, ?)");
-    $newRet->execute([null, $email, 1, $phone, $name]);
+  $userId = getUserId($pdo, $email);
+  if (!$userId) {
+    $userId = addUser($pdo, $email, $phone, $name);
   } else {
+    getOrders($pdo, $email, $phone, $name);
+  }
+  newOrder($pdo, $address, $comment, $email, $userId);
+  $orderResult = getOrderCount($pdo, $email);
+  $usersResult = getUserAnswer($pdo, $userId);
+  echo 'Спасибо, ваш заказ будет доставлен по адресу:', $usersResult['address'], '<br>', 'Номер вашего заказа:', $usersResult['id'], '<br>', 'Это ваш ', $orderResult['orders'], ' заказ';
+}
+
+function addUser ($pdo, $email, $phone, $name) 
+{
+    $newRet = $pdo->prepare("INSERT INTO `users` (`email`, `orders`, `phone`, `name`) VALUES (?, ?, ?, ?)");
+    $newRet->execute([$email, 1, $phone, $name]);
+    return getUserId($pdo, $email);
+}
+
+function getOrders ($pdo, $email, $phone, $name)
+{
     $getOrders = $pdo->prepare("UPDATE `users` SET `orders`=`orders`+ 1, `phone`=?, `name`=? WHERE email=?");
     $getOrders->execute([$phone, $name, $email]);
-  }
-  $date = date("Y-m-d");
-  $getUserId = $pdo->prepare("SELECT `id` FROM `users` WHERE email=?");
-  $getUserId->execute([$email]);
-  $userId = ($getUserId->fetch())[0];
-  $newOrder = $pdo->prepare("INSERT INTO `orders` (`id`, `address`, `comment`, `email`, `user_id`, `date`) VALUES(?, ?, ?, ?, ?, ?)");
-  $newOrder->execute([null, $address, $comment, $email, $userId, $date]);
-  $orderAnswer = $pdo->query("SELECT `id`, `address` FROM `orders` ORDER BY `id` DESC LIMIT 1 ");
-  $orderResult = $orderAnswer->fetch();
-  $usersAnswer = $pdo->prepare("SELECT `orders` FROM `users` WHERE email=?");
-  $usersAnswer->execute([$email]);
-  $usersResult = $usersAnswer->fetch();
-  echo 'Спасибо, ваш заказ будет доставлен по адресу:', $orderResult['address'], '<br>', 'Номер вашего заказа:', $orderResult['id'], '<br>', 'Это ваш ', $usersResult['orders'], ' заказ';
+}
+
+function getUserId ($pdo, $email)
+{
+    $getUserId = $pdo->prepare("SELECT `id` FROM `users` WHERE email=?");
+    $getUserId->execute([$email]);
+    return $userId = ($getUserId->fetch())[0];
+}
+
+function newOrder ($pdo, $address, $comment, $email, $userId)
+{
+    $date = date("Y-m-d");
+    $newOrder = $pdo->prepare("INSERT INTO `orders` (`id`, `address`, `comment`, `email`, `user_id`, `date`) VALUES(?, ?, ?, ?, ?, ?)");
+    $newOrder->execute([null, $address, $comment, $email, $userId, $date]);
+}
+
+function getOrderCount ($pdo, $email)
+{
+      $usersAnswer = $pdo->prepare("SELECT `orders` FROM `users` WHERE email=?");
+      $usersAnswer->execute([$email]);
+      return $usersAnswer->fetch();
+}
+
+function getUserAnswer ($pdo, $userId)
+{
+    $orderAnswer = $pdo->prepare("SELECT `id`, `address` FROM `orders` WHERE `user_id`=?  ORDER BY `id` DESC LIMIT 1 ");
+    $orderAnswer->execute([$userId]);
+    return $orderAnswer->fetch();
 }
